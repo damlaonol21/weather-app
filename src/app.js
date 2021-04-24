ed384d77a28f33641c15bfe8785364fc0aed5d63
damlaonol21 <damlaonol21@gmail.com>
2021-04-24 23:03:53 +0200

    search engine form added

--- src/app.js
+++ src/app.js
@@ -37,12 +37,24 @@ iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.da
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-
+function search(city) {
 
 let apiKey = "dff8bb45a54a7942efbd8b90e1cf34fc";
-let city = "New York"
+
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
+}
+
+search("New York");
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
